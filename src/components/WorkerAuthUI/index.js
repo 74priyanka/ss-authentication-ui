020b7bc6bf04ca458825c94374425c12fa3e690f
priyanka-chauhan <prchauhan0704@gmail.com>
2024-07-27 11:25:53 +0530

    removed button

--- src/components/WorkerAuthUI/index.js
+++ src/components/WorkerAuthUI/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import Login from "../../containers/Login";
 import Signup from "../../containers/Signup";
-import { Button } from "../../remoteComponents";
 
 const WorkerAuthUI = () => {
   return (
     <div>
       <h1>WorkerAuthUI page</h1>
-      <React.Suspense fallback="Loading Header...">
-        <Button />
-      </React.Suspense>
+
       <Login />
       <Signup />
     </div>
